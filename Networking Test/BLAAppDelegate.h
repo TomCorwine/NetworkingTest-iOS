cf09d3e5dad66e5d04f22bc1c14826738c9773b5
Tom Corwine <tc@corwine.org>
2012-07-30 14:58:28 -0400

    Initial Commit

---
+++ Networking Test/BLAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLAAppDelegate.h
+//  Networking Test
+//
+//  Created by Tom Corwine on 7/30/12.
+//  Copyright (c) 2012 Tracks Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Networking Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Networking Test
+//
+//  Created by Tom Corwine on 7/30/12.
+//  Copyright (c) 2012 Tracks Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLAAppDelegate class]));
+	}
+}
